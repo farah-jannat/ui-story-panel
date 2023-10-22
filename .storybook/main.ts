1285aa5b8eb3dd6b921b69ab6b22c3aab9df8c27
“farah-jannat” <farahjannatmithila@gmail.com>
2023-10-22 19:17:33 +0600

    Fix path

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,19 +1,29 @@
+const path = require("path");
+
 import type { StorybookConfig } from "@storybook/nextjs";
 
 const config: StorybookConfig = {
-  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
-  addons: [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-onboarding",
-    "@storybook/addon-interactions",
-  ],
-  framework: {
-    name: "@storybook/nextjs",
-    options: {},
-  },
-  docs: {
-    autodocs: "tag",
-  },
+    stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
+    addons: [
+        "@storybook/addon-links",
+        "@storybook/addon-essentials",
+        "@storybook/addon-onboarding",
+        "@storybook/addon-interactions",
+    ],
+    framework: {
+        name: "@storybook/nextjs",
+        options: {},
+    },
+    docs: {
+        autodocs: "tag",
+    },
+
+    webpackFinal: async (config, { configType }) => {
+        config!.resolve!.alias = {
+            ...config!.resolve!.alias,
+            "@src": path.resolve(__dirname, "../src/"),
+        };
+        return config;
+    },
 };
 export default config;
