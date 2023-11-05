398f15c1d61769e9fab20df7a8ae0617345d39b7
“farah-jannat” <farahjannatmithila@gmail.com>
2023-11-05 10:12:09 +0600

    Format code

--- src/components/ThemeSettings.tsx
+++ src/components/ThemeSettings.tsx
@@ -1,15 +1,21 @@
-import { TooltipComponent } from '@syncfusion/ej2-react-popups'
-import React from 'react'
-import { FiSettings } from 'react-icons/fi'
+import { TooltipComponent } from "@syncfusion/ej2-react-popups";
+import React from "react";
+import { FiSettings } from "react-icons/fi";
 
 const ThemeSettings = () => {
-  return (
-    <div><TooltipComponent content="Settings" position="Top">
-      <button type='button' className='text-3xl p-3 hover:drop-shadow-xl hover: bg-light-gray text-white' style={{background:'blue', borderRadius:'50%'}}><FiSettings/></button>
-      
-      
-      </TooltipComponent></div>
-  )
-}
+    return (
+        <div>
+            <TooltipComponent content="Settings" position="Top">
+                <button
+                    type="button"
+                    className="text-3xl p-3 hover:drop-shadow-xl hover: bg-light-gray text-white"
+                    style={{ background: "blue", borderRadius: "50%" }}
+                >
+                    <FiSettings />
+                </button>
+            </TooltipComponent>
+        </div>
+    );
+};
 
-export default ThemeSettings
+export default ThemeSettings;
