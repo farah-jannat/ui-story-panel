b8d2b2cad9228dfd78339ce9d10dd4627605d248
“farah-jannat” <farahjannatmithila@gmail.com>
2023-11-05 09:57:01 +0600

    Add settings button

--- src/components/ThemeSettings.tsx
+++ src/components/ThemeSettings.tsx
@@ -1,8 +1,14 @@
+import { TooltipComponent } from '@syncfusion/ej2-react-popups'
 import React from 'react'
+import { FiSettings } from 'react-icons/fi'
 
 const ThemeSettings = () => {
   return (
-    <div>ThemeSettings</div>
+    <div><TooltipComponent content="Settings" position="Top">
+      <button type='button' className='text-3xl p-3 hover:drop-shadow-xl hover: bg-light-gray text-white' style={{background:'blue', borderRadius:'50%'}}><FiSettings/></button>
+      
+      
+      </TooltipComponent></div>
   )
 }
 
